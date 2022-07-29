7dee94bd1e272c330d1ca618b0045277d4c9f511
JohannesKarwou <72743318+JohannesKarwou@users.noreply.github.com>
2022-07-29 14:03:01 +0200

    Add files via upload

---
+++ macha/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='macha', #project name
+    version='0.1.0',
+    description='ML',
+    #url
+    author='Me',
+    author_email='charlie_parker@mit.edu', #not a real e-mail
+    license='MIT',
+    packages=find_packages(),
+)
