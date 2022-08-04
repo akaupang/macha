d134b19b18ca776b0f0ce7cac39cb1ab88214846
JohannesKarwou <johannes.karwou@gmail.com>
2022-08-04 16:54:58 +0200

    simple first test

--- macha/tests/test_macha.py
+++ macha/tests/test_macha.py
@@ -1,22 +1,17 @@
-
 import pytest
-from ..functions import check_ligands, Preparation, CharmmManipulation
+from macha.functions import Preparation, check_ligands
 
 
+ligand_id = "../data/templates/cdk2_32.pdb"
+parent_dir = "."
+original_dir = "../data/original"
 
 def test_import():
     import parmed as pm
+    import openbabel
 
 
-def test_preparation():
-
-    ligand_id = "../data/templates/cdk2_32.pdb"
-    parent_dir = "."
-    original_dir = "../data/original"
+def test_createFolders():
 
     preparation = Preparation(parent_dir=parent_dir, ligand_id=ligand_id,original_dir=original_dir,env="waterbox")
-
-    # Make a Transformato style folder structure below a folder bearing
-    # the name of the ligand
     preparation.makeTFFolderStructure()
-    segids = preparation.createCRDfiles()
