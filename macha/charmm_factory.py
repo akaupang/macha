ff938a4e8b32e28b6b8f7df13af8cb4b2c63be14
JohannesKarwou <johannes.karwou@gmail.com>
2023-02-06 09:20:39 +0100

    natsorted list

--- macha/charmm_factory.py
+++ macha/charmm_factory.py
@@ -1,16 +1,16 @@
+import natsort
+
 
 class CharmmFactory:
-
     def __init__():
         pass
-
 
     def createHeader(segids, env):
 
         header = ""
-        for segi in reversed(segids):
+        for segi in natsort.natsorted(segids):
             if segi.startswith("RNA"):
-                header +=f"""
+                header += f"""
 ! Read {segi.upper()}
 open read card unit 10 name {segi.lower()}.crd
 read sequence coor card unit 10 resid
@@ -19,8 +19,15 @@ generate {segi.upper()} setup warn first 5TER last 3TER
 open read unit 10 card name {segi.lower()}.crd
 read coor unit 10 card resid
         """
-            elif env != "waterbox" and segi not in ["HETA","IONS","SOLV","WATA","WATB","WATC"]:
-                header +=f"""
+            elif env != "waterbox" and segi not in [
+                "HETA",
+                "IONS",
+                "SOLV",
+                "WATA",
+                "WATB",
+                "WATC",
+            ]:
+                header += f"""
 ! Read {segi.upper()}
 open read card unit 10 name {segi.lower()}.crd
 read sequence coor card unit 10 resid
@@ -30,7 +37,7 @@ open read unit 10 card name {segi.lower()}.crd
 read coor unit 10 card resid
         """
             elif segi == "HETA":
-                header +=f"""
+                header += f"""
 bomlev -1  ! not ideal but necessary for taking three membered rings into account
 ! Read {segi.upper()}
 open read card unit 10 name {segi.lower()}.crd
